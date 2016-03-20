5488e3894fafef8a830f1aa9eddf098772c4371e
kym.mcgain@ratesetter.com.au <kym.mcgain@ratesetter.com.au>
2016-03-20 18:33:47 +1100

    Added simple ability to do a put request on a key

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var redis = require("redis");
 var app = express();
 var client = redis.createClient();
 
+app.use(require('connect').bodyParser());
+
 app.get('/keys/:pattern?', getKeys);
 app.get('/key/:key', getKeyDetails);
+app.put('/key/:key', putKey);
 
 function getKeys(req, res, next) {
   var pattern = req.params.pattern || "*";
@@ -20,6 +23,21 @@ function getKeyDetails(req, res, next) {
   console.log('getting value of key ' + key);
   client.get(key, function(err, value) {
     res.json(value);
+  });
+}
+
+function putKey(req, res, next) {
+  var key = req.params.key;
+  var value = req.body;
+
+  if (!value.value) {
+    next(new Error("Expecting json: {\"value\": \"...\"} and content type application/json"));
+    return;
+  }
+
+  console.log('setting value of key ' + key);
+  client.set(key, value.value, function(err, redisResponse) {    
+    res.json(redisResponse);
   });
 }
 
